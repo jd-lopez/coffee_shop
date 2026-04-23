e038cd6d0d5731802506e0c4d6aacc54b90e87a0
jd-lopez <jdll0403@gmail.com>
2026-04-23 12:11:21 -0600

    added total to the cart icon in header

--- src/components/cart/CartIcon.jsx
+++ src/components/cart/CartIcon.jsx
@@ -6,12 +6,24 @@ export default function CartIcon() {
   console.log(items);
 
   const totalItems = items.reduce((sum, item) => sum + item.quantity, 0);
+  const totalAmount = items.reduce(
+    (sum, item) => sum + item.price * item.quantity,
+    0,
+  );
+
+  const netTotal = parseFloat(
+    totalAmount + (totalAmount * 0.2 + totalAmount * 0.1),
+  ).toFixed(2);
 
   return (
     <div className="relative">
       <span className="text-2xl">🛒</span>
       <span className="absolute -top-2 left-0 text-red-400 font-bold">
         {totalItems === 0 ? null : totalItems > 9 ? "9+" : totalItems}
+      </span>
+
+      <span className="absolute -bottom-2 left-1 text-red-500 font-bold text-[0.6rem]">
+        {totalItems === 0 ? null : "$" + netTotal}
       </span>
     </div>
   );
