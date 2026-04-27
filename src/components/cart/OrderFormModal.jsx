4ce9a4618d0291b50be72bc5a5b17afccf618afe
jd-lopez <jdll0403@gmail.com>
2026-04-27 00:26:21 -0600

    afa

---
+++ src/components/cart/OrderFormModal.jsx
@@ -0,0 +1,137 @@
+import { motion } from "motion/react";
+import { useState } from "react";
+import { useStore } from "@nanostores/react";
+import { cart } from "../../store/cart";
+
+export default function OrderFormModal({ isFormOpen, setIsFormOpen }) {
+  const [name, setName] = useState("");
+  const [number, setNumber] = useState("");
+  const [address, setAddress] = useState("");
+  const [payment, setPayment] = useState("");
+  console.log(name);
+  function handleSubmition(e) {
+    e.preventDefault();
+
+    const items = cart.get();
+
+    const total = items.reduce(
+      (sum, item) => sum + item.price * item.quantity,
+      0,
+    );
+
+    const itemsText = items
+      .map(
+        (item) =>
+          `- ${item.title} x${item.quantity} — $${(
+            item.price * item.quantity
+          ).toFixed(2)}`,
+      )
+      .join("\n");
+
+    const message = `Hello, I would like to place this order:
+
+Name: ${name}
+Phone: ${number}
+Address: ${address}
+Payment method: ${payment}
+
+Order:
+${itemsText}
+
+Total: $${total.toFixed(2)}
+
+Notes:  "None"`;
+
+    const whatsappUrl = `https://wa.me/+50587952614?text=${encodeURIComponent(
+      message,
+    )}`;
+
+    window.open(whatsappUrl, "_blank");
+  }
+
+  return (
+    <div className="">
+      <div
+        className="absolute inset-0 bg-gray-400/50"
+        onClick={() => setIsFormOpen(false)}
+      ></div>
+      <dialog
+        open
+        className="absolute top-1/7 md:left-1/3 p-4 min-w-60 md:w-120 rounded-2xl overflow-auto"
+      >
+        <form action="" onSubmit={handleSubmition} className="flex flex-col">
+          <h1 className="text-2xl text-primary font-bold">
+            Detalles del pedido
+          </h1>
+          <p className="text-sm">
+            Agrega detalles del pedido como nombre, direccion, forma de pago.
+            Esto sera el contenido del mensaje de whatsapp.
+          </p>
+          <div className="flex flex-col gap-2 mt-4 text-primary font-semibold">
+            <div className="flex flex-col gap-1">
+              <label htmlFor="">Nombre</label>
+              <input
+                type="text"
+                onChange={(e) => setName(e.target.value)}
+                className="border border-gray-600 rounded-lg px-2 py-0.5 outline-0 focus:ring focus:ring-blue-600"
+              />
+            </div>
+
+            <div className="flex flex-col gap-1">
+              <label htmlFor="">Telefono</label>
+              <input
+                type="number"
+                onChange={(e) => setNumber(e.target.value)}
+                className="border border-gray-600 rounded-lg px-2 py-0.5 outline-0 focus:ring focus:ring-blue-600"
+              />
+            </div>
+            <div className="flex flex-col gap-1">
+              <label htmlFor="">Direccion</label>
+              <input
+                type="text"
+                onChange={(e) => setAddress(e.target.value)}
+                className="border border-gray-600 rounded-lg px-2 py-0.5 outline-0 focus:ring focus:ring-blue-600"
+              />
+            </div>
+            <fieldset>
+              <legend>Selecciona un metodo de pago</legend>
+              <div>
+                <input
+                  value="Tarjeta"
+                  type="radio"
+                  name="payment"
+                  onChange={(e) => setPayment(e.target.value)}
+                />
+                <label htmlFor="">Tarjeta</label>
+              </div>
+
+              <div>
+                <input
+                  value="Transferencia"
+                  type="radio"
+                  name="payment"
+                  onChange={(e) => setPayment(e.target.value)}
+                />
+                <label htmlFor="">Transferencia</label>
+              </div>
+
+              <div>
+                <input
+                  value="Efectivo"
+                  type="radio"
+                  name="payment"
+                  onChange={(e) => setPayment(e.target.value)}
+                />
+                <label htmlFor="">Efectivo</label>
+              </div>
+            </fieldset>
+          </div>
+
+          <button className="rounded-2xl bg-primary text-white px-2 py-1 self-center">
+            Order On Whatsapp
+          </button>
+        </form>
+      </dialog>
+    </div>
+  );
+}
